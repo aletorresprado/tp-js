d5995eae0f5743d6e6f9b0ff152f33cc1f651d28
aletorresprado <aletorresprado@live.com>
2025-09-01 13:51:23 -0300

    Update Tps4

--- Tps4.js
+++ Tps4.js
@@ -43,3 +43,71 @@ if (consulta.toLocaleLowerCase() === "si"){
 }
 
 //2
+
+let cuenta = {
+    titular: "Alex",
+    saldo: 0,
+    
+    ingresar: function() {
+    let ingreso = parseInt(prompt("Ingrese el monto a acreditar:"));
+    
+    if (isNaN(ingreso) || ingreso <= 0) {
+        alert("Monto inválido. Debe ser un número positivo.");
+        return;
+    }
+    
+    this.saldo += ingreso;
+    this.informar(`Se acreditaron $${ingreso}`);
+    
+    console.log(`Se acreditaron $${ingreso}`);
+    console.log(`El nuevo saldo de ${cuenta.titular} es de: $${cuenta.saldo}`);
+    alert(`Se acreditaron $${ingreso}\n El nuevo saldo de ${cuenta.titular} es de: $${cuenta.saldo}`);
+    },
+
+    extraer: function(){
+    let egreso = parseInt(prompt("Ingrese el monto a extraer:"));  
+    
+    if (isNaN(egreso) || egreso <= 0) {
+        alert("Monto inválido. Debe ser un número positivo.");
+        return;
+
+    }if (egreso > this.saldo){
+        alert("Saldo insuficiente.");
+        return;
+    }
+    
+    this.saldo -= egreso;
+    this.informar(`Se extrajeron $${egreso}`);
+
+    console.log(`Se extrayeron $${egreso}`);
+    console.log(`El nuevo saldo de ${cuenta.titular} es de: $${cuenta.saldo}`);
+    alert(`Se extrayeron $${egreso}\n El nuevo saldo de ${cuenta.titular} es de: $${cuenta.saldo}`);
+    },
+    
+    informar: function(mensaje = "Estado actual de la cuenta") {
+        console.log(`${mensaje}\nTitular: ${this.titular}\nSaldo: $${this.saldo}`);
+        alert(`${mensaje}\nTitular: ${this.titular}\nSaldo: $${this.saldo}`);
+    }
+};
+
+
+// Menú con bucle este cambio lo sumo por ia ya que perdia la memoria 
+/// lo voy a estudiar para implementar
+
+let opcion;
+do {
+    opcion = prompt("Bienvenido, ¿qué deseas hacer? (Ingresar, Extraer, Informar, Salir)").toLowerCase();
+    
+    if (opcion === "ingresar") {
+        cuenta.ingresar();
+    } else if (opcion === "extraer") {
+        cuenta.extraer();
+    } else if (opcion === "informar") {
+        cuenta.informar();
+    } else if (opcion !== "salir") {
+        alert("Elige una opción correcta (Ingresar, Extraer, Informar o Salir).");
+    }
+
+} while (opcion !== "salir");
+
+alert("Gracias por usar el sistema");
